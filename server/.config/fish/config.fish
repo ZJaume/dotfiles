04b5a6650ffaa17989653f8e4114536d34670a53
ZJaume <jaumet@posteo.net>
2021-01-26 17:21:40 +0100

    Move fish config to server

--- server/.config/fish/config.fish
+++ server/.config/fish/config.fish
@@ -1,5 +1,5 @@
 alias battery "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep 'percentage\|state'"
 
-if status is-interactive
+if status is-interactive; and test -s ~/.cache/wal/sequences
     cat ~/.cache/wal/sequences &
 end
