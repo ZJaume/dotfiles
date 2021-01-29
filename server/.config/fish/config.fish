05e7e2270f423f7d438e0d820c6dbb34d991d383
ZJaume <jzaragoza@prompsit.com>
2021-01-29 10:03:20 +0000

    Load conda if installed

--- server/.config/fish/config.fish
+++ server/.config/fish/config.fish
@@ -4,3 +4,11 @@ alias battery "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep 'pe
 if status is-interactive; and test -s ~/.cache/wal/sequences
     cat ~/.cache/wal/sequences &
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -d /home/jzaragoza/miniconda3
+    eval /home/jzaragoza/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
