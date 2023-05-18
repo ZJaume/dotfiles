2f7deecf3b63009f3eb535bf8a4def0816ea26a1
ZJaume <jzaragoza@prompsit.com>
2023-05-18 10:33:08 +0200

    Remove ssh sockets alias

--- server/.config/fish/config.fish
+++ server/.config/fish/config.fish
@@ -3,6 +3,7 @@ alias sv "source venv/bin/activate.fish"
 alias awk "awk -F\t"
 alias pzcat "pigz -dc"
 alias tmpclean "find /tmp/ -user $USER -name tmp\* -exec rm -r "{}" \;"
+alias rmsocks "rm ~/.ssh/sockets/socket-*"
 alias battery "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep 'percentage\|state'"
 
 if status is-interactive; and test -s ~/.cache/wal/sequences
