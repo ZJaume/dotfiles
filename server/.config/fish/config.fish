7cec917cdc0f1c1c96f8e98b7ecd4c3c657b72e8
ZJaume <jzaragoza@prompsit.com>
2023-12-12 11:37:42 +0100

    Use paru instead of yay

--- server/.config/fish/config.fish
+++ server/.config/fish/config.fish
@@ -6,6 +6,7 @@ alias noempty "grep -v '^[[:blank:]]*\$'"
 alias tmpclean "find /tmp/ -user $USER -name tmp\* -exec rm -r "{}" \;"
 alias rmsocks "rm ~/.ssh/sockets/socket-*"
 alias battery "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep 'percentage\|state'"
+alias yay "paru"
 
 if status is-interactive; and test -s ~/.cache/wal/sequences
     cat ~/.cache/wal/sequences &
